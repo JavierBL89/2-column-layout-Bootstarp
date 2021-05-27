767eee9d8f65322483afb2860e77664845bec3be
JavierBL89 <74215386+JavierBL89@users.noreply.github.com>
2021-05-27 22:19:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,4 +9,15 @@ var textInputSytle = $("input").click(function(){
   changeInputStyle.css("font-size", "0.8rem" );
  $(this).attr("value", "");
 
+
+
+});
+
+
+var mobileNavbar = $(".navbar-toggler").on("click", function(){
+  var navbarMobile = $(".navbar").addClass("navbar-mobile");
+  var mobileNavbarLink = $(".nav-link").addClass("mobile-nav-link");
+  var textHeaddinContainer = $(".heading").addClass("mobile-heading");
+  var mobileNavbarBackgound = $(".navbar-nav").addClass("mobile-navbar-background");
+
 })
